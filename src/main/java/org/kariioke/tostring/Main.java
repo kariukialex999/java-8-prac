11d0cf605c71d0346ba095725e85ef42a857f513
alex kariuki <kariukialex999@gmail.com>
2026-04-01 14:52:26 +0300

    understanding the toString function

---
+++ src/main/java/org/kariioke/tostring/Main.java
@@ -0,0 +1,8 @@
+package org.kariioke.tostring;
+
+public class Main {
+    public void main () {
+        ToString toString = new ToString("Alex", "Kariioke1");
+        System.out.println(toString);
+    }
+}
--- src/main/java/org/kariioke/tostring/ToString.java
+++ src/main/java/org/kariioke/tostring/ToString.java
@@ -1,4 +1,20 @@
 package org.kariioke.tostring;
 
+
 public class ToString {
+    private String name;
+    private String password;
+
+    ToString(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
